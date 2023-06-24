1bd8fe55150694ef40dc8f43a0bfd697d5e1e9f7
robolev <92596597+robolev@users.noreply.github.com>
2023-06-24 11:46:04 +0200

    Sound

--- Engine/Sound/SoundPlayer.cs
+++ Engine/Sound/SoundPlayer.cs
@@ -4,22 +4,22 @@ public class SoundPlayer
 {
     public List<string> AudioClipsList = new List<string>();
     
-    private static Dictionary<string, SFML.Audio.Sound> audioClips = new Dictionary<string, SFML.Audio.Sound>();
+    private static Dictionary<string, SFML.Audio.Sound> soundClips = new Dictionary<string, SFML.Audio.Sound>();
 
     public void LoadAudioClips()
     {
-        audioClips.Clear();
+        soundClips.Clear();
         foreach(string pathToClip in AudioClipsList)
         {
             string name = pathToClip.Split('\\').Last().Split('.').First();
             
-            audioClips.Add(name, new SFML.Audio.Sound(new SFML.Audio.SoundBuffer(pathToClip)));
+            soundClips.Add(name, new SFML.Audio.Sound(new SFML.Audio.SoundBuffer(pathToClip)));
         }
     }
     
     public static void PlayAudioClip(string name, bool loop = false)
     {
-        if (audioClips.TryGetValue(name, out var audioClip))
+        if (soundClips.TryGetValue(name, out var audioClip))
         {
             audioClip.Loop = loop;
             audioClip.Play();
@@ -28,7 +28,7 @@ public class SoundPlayer
     
     public void SetVolume(float volume) 
     {
-        foreach (var audioClip in audioClips)
+        foreach (var audioClip in soundClips)
         {
             audioClip.Value.Volume = volume;
         }
@@ -36,7 +36,7 @@ public class SoundPlayer
 
     public static SFML.Audio.Sound GetSoundByName(string name)
     {
-        if (audioClips.TryGetValue(name, out var audioClip))
+        if (soundClips.TryGetValue(name, out var audioClip))
         {
             return audioClip;
         }
